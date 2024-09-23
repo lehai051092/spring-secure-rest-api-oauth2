6f3265df7da1bffb14698211e13bed7c91423d3f
HaiLT <bibo051092@gmail.com>
2024-09-23 13:26:45 +0700

    Adding data authorization

--- src/main/java/example/springsecurerestapioauth2/model/repositories/CashCardRepository.java
+++ src/main/java/example/springsecurerestapioauth2/model/repositories/CashCardRepository.java
@@ -1,19 +1,19 @@
 package example.springsecurerestapioauth2.model.repositories;
 
 import example.springsecurerestapioauth2.model.CashCard;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.lang.NonNull;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 public interface CashCardRepository extends CrudRepository<CashCard, Long> {
     Iterable<CashCard> findByOwner(String owner);
 
-    default Iterable<CashCard> findAll() {
-        SecurityContext context = SecurityContextHolder.getContext();
-        Authentication authentication = context.getAuthentication();
-        String owner = authentication.getName();
-        return findByOwner(owner);
-    }
+    @NonNull
+    @Query("select * from cash_card cc where cc.owner = :#{authentication.name}")
+    Iterable<CashCard> findAll();
+
+//    @NonNull
+//    default Iterable<CashCard> findAll() {
+//        throw new UnsupportedOperationException("unsupported, please use findByOwner instead");
+//    }
 }
