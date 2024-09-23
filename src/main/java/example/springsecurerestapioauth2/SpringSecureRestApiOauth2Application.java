b6323cfb9b1db412ab04d0e2d65fc3355f5073d8
HaiLT <bibo051092@gmail.com>
2024-09-23 11:00:30 +0700

    Adding method authorization

--- src/main/java/example/springsecurerestapioauth2/SpringSecureRestApiOauth2Application.java
+++ src/main/java/example/springsecurerestapioauth2/SpringSecureRestApiOauth2Application.java
@@ -4,6 +4,7 @@ import example.springsecurerestapioauth2.component.ProblemDetailsAuthenticationE
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,18 +12,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @SpringBootApplication
 public class SpringSecureRestApiOauth2Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringSecureRestApiOauth2Application.class, args);
-    }
-
     @Bean
     SecurityFilterChain appSecurity(HttpSecurity http, ProblemDetailsAuthenticationEntryPoint entryPoint) throws Exception {
         http
-                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.GET, "/cashcards/**").hasAuthority("SCOPE_cashcard:read")
+                        .requestMatchers("/cashcards/**").hasAuthority("SCOPE_cashcard:write")
+                        .anyRequest().authenticated()
+                )
                 .oauth2ResourceServer((oauth2) -> oauth2
                         .authenticationEntryPoint(entryPoint)
                         .jwt(Customizer.withDefaults())
                 );
         return http.build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecureRestApiOauth2Application.class, args);
     }
 }
